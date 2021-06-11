4843091023dc6ff4387291fb61dd29f56185f1ad
DuhyeongKim <21400080@handong.edu>
2021-06-11 21:28:04 +0900

    second frf

--- lib/profile.dart
+++ lib/profile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:treeplanet/payment.dart';
 
 import 'login.dart';
 import 'product.dart';
@@ -17,6 +18,7 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   int _selectedIndex = 4;
+  int total = 0;
 
   Future<bool> docExist() async {
     FirebaseFirestore cart = FirebaseFirestore.instance;
@@ -34,9 +36,10 @@ class _ProfilePageState extends State<ProfilePage> {
     return Container(
       decoration: BoxDecoration(
           image: DecorationImage(
-            fit: BoxFit.cover,
-            image: AssetImage('images/groot.jpg'),
+            image: AssetImage('images/plant.png'),
+            fit: BoxFit.cover
           )),
+
       child: Scaffold(
       backgroundColor: Colors.transparent,
       appBar: AppBar(
@@ -109,6 +112,20 @@ class _ProfilePageState extends State<ProfilePage> {
               ),
             ),
           ),
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Cart',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
           FutureBuilder(
             future: docExist(),
             builder: (context, snapshot) {
@@ -128,52 +145,163 @@ class _ProfilePageState extends State<ProfilePage> {
                             ProductsRepository.loadProducts(Category.all);
 
                         return Container(
-                          height: 600.0,
+
                           child: ListView(
+
+
                             shrinkWrap: true,
                             padding: const EdgeInsets.all(0.0),
                             scrollDirection: Axis.vertical,
+
                             children: ids
                                 .map(
                                   (e) => Container(
+
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image: AssetImage('images/groom.jpg'),
+                                            fit: BoxFit.cover
+                                        )),
                                     alignment: Alignment.center,
-                                    child: Padding(
-                                      padding: EdgeInsets.all(10.0),
-                                      child: OutlinedButton(
-                                        child: Padding(
-                                          padding: EdgeInsets.all(15.0),
+                                    child:
+                                      Column(
+                                      children: <Widget>[
+                                        Padding(
+                                          padding: EdgeInsets.all(10.0),
+                                            child: Padding(
+                                              padding: EdgeInsets.all(5.0),
                                           child: Text(
                                             products[e].name,
                                             style: TextStyle(
-                                              fontSize: 15.0,
+                                              fontSize: 20.0,
                                               fontWeight: FontWeight.bold,
                                               color: Colors.white,
                                             ),
                                           ),
                                         ),
-                                        style: OutlinedButton.styleFrom(
-                                          primary: Colors.white,
-                                          backgroundColor: Colors.teal,
-                                          side: BorderSide(
-                                              color: Colors.red, width: 5),
-                                        ),
-                                      ),
-                                    ),
+
                                   ),
-                                )
-                                .toList(),
+
+
+                                    SizedBox(width: 10.0),
+                                       Divider(),
+                                       Row(
+                                         mainAxisAlignment: MainAxisAlignment.center,
+                                         crossAxisAlignment: CrossAxisAlignment.start,
+                                         children: <Widget>[
+
+                                           Text(
+                                             products[e].price.toString(),
+                                             style: TextStyle(
+                                               fontSize: 20.0,
+                                               fontWeight: FontWeight.bold,
+                                               color: Colors.white,
+                                             ),
+                                           ),
+                                          SizedBox(width: 20.0,),
+
+
+                                           Container(
+                                               width: 20.0,
+                                               height: 20.0,
+                                               decoration: BoxDecoration(
+                                                 color: Colors.blue[300],
+                                                 borderRadius: BorderRadius.circular(4.0),),
+                                               child: Icon(
+                                                 Icons.add,
+                                                 color: Colors.white,
+                                                 size: 15.0,
+                                               )
+                                           ),
+
+                                           Padding(
+                                             padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                                               child: Text(
+                                                 "1",
+                                                 style: TextStyle(
+                                                   fontSize: 18.0,
+                                                   fontWeight: FontWeight.bold,
+                                                   color: Colors.black,
+                                                 ),
+                                               ),
+                                           ),
+
+
+                                        Container(
+                                          width: 20.0,
+                                          height: 20.0,
+                                          decoration: BoxDecoration(
+                                            color: Colors.grey[300],
+                                            borderRadius: BorderRadius.circular(4.0),),
+                                          child: Icon(
+                                            Icons.add,
+                                            color: Colors.white,
+                                            size: 15.0,
+                                          )
+                                        ),
+
+
+                                       ]),
+
+
+                                    ]),
+
+
+                                  )).toList(),
+
                           ),
+
                         );
                       }
                     },
                   );
                 } else {
-                  return Container(height: 600);
+                  return Container(height: 800);
                 }
               } else {
                 return Center(child: CircularProgressIndicator());
               }
             },
+          ),
+
+        Divider(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              "Total",
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              total.toString(),
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ],
+        ),
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: RaisedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MainCard()),
+                );
+              },
+              elevation: 0.5,
+              color: Colors.cyan,
+              child: Center(
+                child: Text(
+                  'Pay Now',
+                ),
+              ),
+            ),
           ),
           logoutButton(),
         ],
@@ -194,9 +322,9 @@ class _ProfilePageState extends State<ProfilePage> {
         );
       },
       child: Container(
-          color: Colors.orange,
+          color: Colors.indigoAccent,
           child: Padding(
-            padding: const EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(8.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
